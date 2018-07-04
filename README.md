34be1403ffdfbc1a85f8742d33b6e08f48deba47
wwendyc <wendy.changw@gmail.com>
2018-07-04 17:35:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Level 2 React App
+
+### Built with
+* Create React App
+* React
+* React Spring
+
+### Elements
+* Render props
+* Fragments
+* Reusable portal
+* React Context API
+* React Spring transitions
+* Animated draggable components
+* Gesture-based interface
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Toggle from './ToggleRenderProps'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Toggle
+          render={({on, toggle}) => (
+            <div>
+              {on && <h1>Show Me</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && <nav>nav item</nav>}
+              <button onClick={toggle}>Menu</button>
+            </div>
+          )}
+        />
+      </div>
+    )
+  }
+}
+
+export default App
